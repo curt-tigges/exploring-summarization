d34eb36d78e687ea5e73f75d1f9ebff7d6f560b3
skar0 <oskar.hollinsworth@gmail.com>
2023-10-30 16:50:42 +0000

    Added tests for zero_attention_pos_hook and get_layerwise_mean_acts

--- tests/test_tokenwise_ablation.py
+++ tests/test_tokenwise_ablation.py
@@ -3,16 +3,42 @@ from unittest.mock import patch, Mock
 import torch
 import numpy as np
 from typing import List
+from torch.utils.data import DataLoader
 from transformer_lens import HookedTransformer
+from transformer_lens.utils import get_act_name
 from utils.tokenwise_ablation import (
     find_positions,
     load_directions,
     get_random_directions,
     get_zeroed_dir_vector,
+    get_layerwise_token_mean_activations,
+    zero_attention_pos_hook,
 )
 
 
-class TestUtils(unittest.TestCase):
+class HookPoint:
+    def __init__(self, name=None):
+        self.name = name
+
+
+# More Mock objects and constants for testing
+class MockBatch:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+
+class MockDataLoader(DataLoader):
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+
+class TestTokenwise(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.model = HookedTransformer.from_pretrained("gelu-1l")
@@ -37,6 +63,37 @@ class TestUtils(unittest.TestCase):
     def test_get_zeroed_dir_vector(self):
         result = get_zeroed_dir_vector(self.model)
         expected_result = torch.zeros((1, 512))
+        torch.testing.assert_close(result, expected_result)
+
+    @patch.object(HookedTransformer, "run_with_cache")
+    def test_get_layerwise_token_mean_activations(self, mock_run_with_cache):
+        # Mocking the return value for `model.run_with_cache`
+        mock_run_with_cache.return_value = (
+            None,
+            {
+                "blocks.0.hook_resid_post": torch.tensor(
+                    [[[1], [2], [3], [4]], [[5], [6], [7], [8]]]
+                ),
+            },
+        )
+        self.model.cfg.d_model = 1
+
+        data_loader = MockDataLoader(
+            [
+                {"tokens": torch.tensor([[1, 2, 3, 4], [5, 6, 2, 7]])},
+            ]
+        )
+        result = get_layerwise_token_mean_activations(
+            self.model, data_loader, token_id=2
+        )
+        expected_result = torch.tensor([[4.5]])
+        torch.testing.assert_close(result, expected_result)
+
+    def test_zero_attention_pos_hook(self):
+        pattern = torch.tensor([[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]])
+        hook = self.model.hook_dict[get_act_name("pattern", 0)]
+        result = zero_attention_pos_hook(pattern, hook, pos_by_batch=[[1]], head_idx=1)
+        expected_result = torch.tensor([[[[1, 2], [3, 4]], [[5, 6], [7, 0]]]])
         torch.testing.assert_close(result, expected_result)
 
 
