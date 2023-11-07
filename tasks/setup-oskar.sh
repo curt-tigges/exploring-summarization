ffcf31bbed003343a2ef0ee68805d8c2278d85b4
skar0 <oskar.hollinsworth@gmail.com>
2023-11-07 11:13:21 +0000

    Changed github username

--- tasks/setup-oskar.sh
+++ tasks/setup-oskar.sh
@@ -1,3 +1,3 @@
 git config --global user.email "oskar.hollinsworth@gmail.com" && \
-    git config --global user.name "skar0" && \
+    git config --global user.name "ojh31" && \
     git config --global remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
