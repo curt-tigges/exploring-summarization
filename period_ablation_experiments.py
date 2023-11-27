df8d582c273500a4dd39fcbe42ea1735032140a0
Curt Tigges <curttigges@pop-os.localdomain>
2023-11-27 15:51:04 -0800

    Various misc updates

--- period_ablation_experiments.py
+++ period_ablation_experiments.py
@@ -40,26 +40,14 @@ from IPython.display import HTML, display
 from utils.circuit_analysis import get_logit_diff
 
 from utils.tokenwise_ablation import (
-<<<<<<< HEAD
-<<<<<<< HEAD
     compute_ablation_modified_loss,
-=======
-    compute_ablation_modified_metric,
->>>>>>> 59a3578 (Started analyzing period ablations)
-=======
-    compute_ablation_modified_loss,
->>>>>>> c1230bf (Removed reference to masking function in compute_ablation_modified_loss)
     load_directions,
     get_random_directions,
     get_zeroed_dir_vector,
     get_layerwise_token_mean_activations,
 )
 from utils.datasets import OWTData, PileFullData, PileSplittedData
-<<<<<<< HEAD
 from utils.neuroscope import plot_top_onesided
-=======
-from utils.neuroscope import plot_topk_onesided
->>>>>>> 59a3578 (Started analyzing period ablations)
 
 # %%
 device = torch.device("cuda")
@@ -77,7 +65,7 @@ assert isinstance(TOKEN_ID, int)
 print(TOKEN_ID)
 # %%
 exp_data = OWTData.from_model(model)
-exp_data.preprocess_datasets(token_to_ablate=TOKEN_ID)
+exp_data.preprocess_datasets(token_to_ablate=TOKEN_ID, max_length=64)
 # %%
 data_loader = exp_data.get_dataloaders(batch_size=BATCH_SIZE)[SPLIT]
 print(data_loader.name)
@@ -86,10 +74,6 @@ comma_mean_values = get_layerwise_token_mean_activations(
     model, data_loader, token_id=TOKEN_ID, device=device
 )
 # %%
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c1230bf (Removed reference to masking function in compute_ablation_modified_loss)
 smaller_owt = OWTData.from_model(model)
 smaller_owt.dataset_dict[SPLIT] = smaller_owt.dataset_dict[SPLIT].select(
     list(range(100))
@@ -98,36 +82,16 @@ smaller_owt.preprocess_datasets(token_to_ablate=TOKEN_ID)
 smaller_data_loader = smaller_owt.get_dataloaders(batch_size=BATCH_SIZE)[SPLIT]
 
 # %%
-<<<<<<< HEAD
 losses = compute_ablation_modified_loss(
     model,
     smaller_data_loader,
     cached_means=comma_mean_values,
-=======
-=======
->>>>>>> c1230bf (Removed reference to masking function in compute_ablation_modified_loss)
-losses = compute_ablation_modified_metric(
-    model,
-    smaller_data_loader,
-    cached_means=comma_mean_values,
-    metric="loss",
->>>>>>> 59a3578 (Started analyzing period ablations)
     device=device,
 )
 # %%
 ablated_loss_diffs = losses[1]
 losses.shape
 # %%
-<<<<<<< HEAD
-plot_top_onesided(ablated_loss_diffs, smaller_data_loader, model, k=10)
-# %%
-plot_top_onesided(
-    ablated_loss_diffs,
-    smaller_data_loader,
-    model,
-    k=10,
-    p=0.1,
-=======
 plot_topk_onesided(
     ablated_loss_diffs,
     smaller_data_loader,
@@ -140,7 +104,7 @@ plot_top_p(
     smaller_data_loader,
     model,
     k=10,
->>>>>>> 59a3578 (Started analyzing period ablations)
+    p=0.1,
     window_size=50,
 )
 
@@ -152,17 +116,10 @@ ablated_loss_diffs[0][15:30]
 
 # %%
 batch = next(iter(data_loader))
-<<<<<<< HEAD
-batch["tokens"][0][15:30]
-
-# %%
-model.to_str_tokens(batch["tokens"][0][15:30])
-=======
 batch['tokens'][0][15:30]
 
 # %%
 model.to_str_tokens(batch['tokens'][0][15:30])
->>>>>>> 59a3578 (Started analyzing period ablations)
 
 # %%
 exp_data = OWTData.from_model(model)
@@ -171,10 +128,6 @@ exp_data = OWTData.from_model(model)
 ds = exp_data.get_datasets()
 
 # %%
-<<<<<<< HEAD
-ds["train"][1]["text"]
-=======
 ds['train'][1]['text']
->>>>>>> 59a3578 (Started analyzing period ablations)
 
 # %%
