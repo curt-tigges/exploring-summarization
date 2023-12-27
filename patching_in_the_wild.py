1bf82339e5db18c2c3c91f95cb694a07464533f1
skar0 <oskar.hollinsworth@gmail.com>
2023-12-27 14:08:57 +0000

    Switched to full batch results

--- patching_in_the_wild.py
+++ patching_in_the_wild.py
@@ -71,16 +71,11 @@ from summarization_utils.counterfactual_patching import (
 # %%
 SPLIT = "train"
 DATA_FILES = [
-    # "https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/ArXiv/train/data-00000-of-00222.arrow",
-    # "https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/BookCorpus2/train/data-00000-of-00096.arrow",
-    "https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/Github/train/data-00000-of-00191.arrow"
-    # "https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/Gutenberg%20(PG-19)/train/data-00000-of-00096.arrow",
-    # "https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/PhilPapers/train/data-00000-of-00096.arrow",
-    # "https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/PubMed%20Abstracts/train/data-00000-of-00096.arrow",
-    # "https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/Wikipedia%20(en)/train/data-00000-of-00101.arrow",
+    f"https://huggingface.co/datasets/ArmelR/the-pile-splitted/blob/main/data/Github/train/data-{i:05d}-of-00191.arrow"
+    for i in range(10)
 ]
 NAME = DATA_FILES[0].split("/")[-3].replace("%20", " ")
-BATCH_SIZE = 64
+BATCH_SIZE = 1024
 # %%
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
@@ -112,13 +107,33 @@ exp_data.preprocess_datasets(token_to_ablate=ablation_token)
 # %%
 data_loader = exp_data.get_dataloaders(batch_size=BATCH_SIZE)[SPLIT]
 # %%
-template = "for i in range(x"  # x: \n    print( TODO: start simple here and then move to more complex
+template = "for i in range(1, len(sys.argv)):\n   "
 tokens = model.to_tokens(template, prepend_bos=False)
 str_tokens = model.to_str_tokens(tokens, prepend_bos=False)
+print([f"{s}:{t}" for s, t in zip(str_tokens, tokens[0].tolist())])
 search_tokens = [
-    t if "x" not in s else -1 for t, s in zip(tokens.tolist()[0], str_tokens)
+    996,  # for
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    3554,  # )):
+    258,  # 258 is 3, 246 is 4 spaces
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
 ]
-print(search_tokens)
 # %%
 
 
@@ -127,6 +142,7 @@ def find_subtensor(
     sub_tensor: torch.Tensor,
     wildcard_value: int = -1,
     device: torch.device = torch.device("cuda"),
+    return_full_batch: bool = False,
 ) -> torch.Tensor:
     # Move tensors to GPU
     main_tensor = main_tensor.to(device)
@@ -141,6 +157,8 @@ def find_subtensor(
     sub_tensors = main_tensor.unfold(1, sub_tensor.size(0), 1)
     is_match = sub_tensors == sub_tensor
     indices = torch.all(is_match | is_wildcard, dim=2)
+    if return_full_batch:
+        return torch.where(indices)[0]
     result = torch.masked_select(sub_tensors, indices.unsqueeze(2)).view(
         -1, sub_tensor.size(0)
     )
@@ -152,16 +170,22 @@ search_function = partial(
     find_subtensor,
     sub_tensor=torch.tensor(search_tokens, device=device),
     wildcard_value=-1,
+    return_full_batch=True,
 )
 # %%
 MAX_RESULTS = 100
 results = []
-for batch in tqdm(data_loader, total=len(data_loader)):
+for batch_num, batch in enumerate(tqdm(data_loader, total=len(data_loader))):
     for result in search_function(batch["tokens"]):
-        results.append(result)
+        results.append(batch_num * BATCH_SIZE + result)
     if len(results) >= MAX_RESULTS:
         break
 # %%
-for result in results:
-    print(model.to_str_tokens(result))
+print(len(results))
 # %%
+for result in results:
+    tokens = exp_data.dataset_dict[SPLIT][result.item()]["tokens"]
+    print(model.to_string(tokens))
+
+# %%
+# TODO: try to get O(100) results and then find a way to pair them up and truncate them nicely
