1d268c49202b30a4dadf2caf450d211137c34c38
skar0 <oskar.hollinsworth@gmail.com>
2023-12-29 15:57:56 +0000

    Forked toy dataset script

---
+++ toy_dataset_circuit_analysis.py
@@ -0,0 +1,184 @@
+# %%
+import itertools
+import random
+import einops
+from functools import partial
+import numpy as np
+import torch
+import datasets
+import os
+import re
+from torch import Tensor
+from torch.utils.data import DataLoader
+from datasets import load_dataset, concatenate_datasets
+from jaxtyping import Float, Int, Bool
+from typing import Dict, Iterable, List, Tuple, Union, Literal, Optional
+from transformer_lens import HookedTransformer
+from transformer_lens.utils import (
+    get_dataset,
+    tokenize_and_concatenate,
+    get_act_name,
+    test_prompt,
+    get_attention_mask,
+)
+from transformer_lens.hook_points import HookPoint
+from tqdm.notebook import tqdm
+import pandas as pd
+from circuitsvis.activations import text_neuron_activations
+from circuitsvis.topk_samples import topk_samples
+from IPython.display import HTML, display
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from summarization_utils.patching_metrics import get_logit_diff
+from summarization_utils.tokenwise_ablation import (
+    compute_ablation_modified_loss,
+    load_directions,
+    get_random_directions,
+    get_zeroed_dir_vector,
+    get_layerwise_token_mean_activations,
+    ablation_hook_base,
+    AblationHook,
+    AblationHookIterator,
+    get_batch_token_mean_activations,
+    loss_fn,
+    DEFAULT_DEVICE,
+)
+from summarization_utils.datasets import (
+    OWTData,
+    PileFullData,
+    PileSplittedData,
+    HFData,
+    mask_positions,
+    construct_exclude_list,
+)
+from summarization_utils.neuroscope import plot_top_onesided
+from summarization_utils.store import ResultsFile, TensorBlockManager
+from summarization_utils.path_patching import act_patch, Node, IterNode, IterSeqPos
+from summarization_utils.toy_datasets import (
+    CounterfactualDataset,
+    ToyDeductionTemplate,
+    ToyBindingTemplate,
+    ToyProfilesTemplate,
+)
+from summarization_utils.counterfactual_patching import (
+    patch_by_position_group,
+    patch_by_layer,
+    plot_layer_results_per_batch,
+    plot_head_results_per_batch,
+)
+from summarization_utils.visualization import (
+    plot_attention,
+    scatter_attention_and_contribution,
+    scatter_attention_and_contribution_sentiment,
+    scatter_attention_and_contribution_simple,
+    scatter_attention_and_contribution_logic,
+)
+
+# %%
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+os.environ["TORCH_USE_CUDA_DSA"] = "1"
+torch.set_grad_enabled(False)
+torch.manual_seed(0)
+random.seed(0)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# %%
+model: HookedTransformer = HookedTransformer.from_pretrained(
+    "santacoder",
+    torch_dtype=torch.float32,
+    fold_ln=False,
+    center_writing_weights=False,
+    center_unembed=False,
+    device=device,
+)
+assert model.tokenizer is not None
+
+
+# %%
+def increment_list(l: List[int]) -> List[int]:
+    for i in range(len(l)):
+        l[i]
+
+
+# %%
+CODE_TUPLES = [
+    # (
+    #     "def print_first_n_factorial_inorder(n: int) -> None:\n    x = 1\n    for num in range(1, n + 1):\n        x = x",
+    #     " *",
+    #     "def print_first_n_triangular_numbers(n: int) -> None:\n    x = 0\n    for num in range(1, n + 1):\n        x = x",
+    #     " +",
+    # ),  # 29% at "x"
+    (
+        "def print_first_n_composites(n: int) -> None:\n    for num in range(2, n):\n        if num > 1:\n            for i in range(2, num):\n                if (num % i) == 0:\n                    ",
+        " print",
+        "def print_first_n_prime_numbers(n: int) -> None:\n    for num in range(2, n):\n        if num > 1:\n            for i in range(2, num):\n                if (num % i) == 0:\n                    ",
+        " break",
+    ),  # 52% at ":"
+    (
+        "def Factorial(n: int) -> int:\n    if n <= 1:\n        return ",
+        "1",
+        "def fibonacci(n: int) -> int:\n    if n <= 0:\n        return ",
+        "0",
+    ),  # (25% at "return", ) 16% at ":"
+    # (
+    #     "def is_even(n: int) -> bool:\n    if n % 2 == ",
+    #     "0",
+    #     "def is_odd(n: int) -> bool:\n    if n % 2 == ",
+    #     "1",
+    # ),  # 15% at "=="
+    # (
+    #     "def increment_list(l: List[int]) -> List[int]:\n    for i in range(len(l)):\n        l[i] = l[i]",
+    #     " +",
+    #     "def decrement_list(l: List[int]) -> List[int]:\n    for i in range(len(l)):\n        l[i] = l[i]",
+    #     " -",
+    # ),
+    # (
+    #     "def increment_list(l: List[int]) -> List[int]:\n    for value in l:\n        value = value",
+    #     " +",
+    #     "def decrement_list(l: List[int]) -> List[int]:\n    for value in l:\n        value = value",
+    #     " -",
+    # ),
+]
+patch_positions = torch.tensor(
+    [
+        # 39,
+        56,
+        18,
+        # 19,
+        # 24
+    ],
+    dtype=torch.long,
+    device=device,
+).unsqueeze(1)
+# %%
+for i, (prompt, _, _, _) in enumerate(CODE_TUPLES):
+    print(model.to_str_tokens(prompt)[patch_positions[i]])
+    # print([f"{i}:{t}" for i, t in enumerate(model.to_str_tokens(prompt))])
+# %%
+dataset = CounterfactualDataset.from_tuples(CODE_TUPLES, model)
+# %%
+dataset.check_lengths_match()
+# %%
+dataset.test_prompts(max_prompts=10, top_k=10)
+# %%
+all_logit_diffs, cf_logit_diffs = dataset.compute_logit_diffs()
+# %%
+print(f"Original mean: {all_logit_diffs.mean():.2f}")
+print(f"Counterfactual mean: {cf_logit_diffs.mean():.2f}")
+# %%
+assert (all_logit_diffs > 0).all()
+assert (cf_logit_diffs < 0).all()
+# # %%
+# pos_layer_results = patch_by_layer(dataset)
+# print(len(pos_layer_results), pos_layer_results[0].shape)
+# # %%
+# plot_layer_results_per_batch(dataset, pos_layer_results)
+# %%
+head_layer_results = patch_by_layer(dataset, node_name="z", seq_pos=patch_positions)
+print(len(head_layer_results), head_layer_results[0].shape)
+
+
+# %%
+plot_head_results_per_batch(dataset, head_layer_results)
+# %%
