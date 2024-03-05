eaea146f943c9fbcef24df4d63eaba24b317a4e8
skar0 <oskar.hollinsworth@gmail.com>
2024-03-05 19:10:51 +0000

    First attempt at templatic walked-to dataset. Mistral cannot get 100% accuracy.

--- summarization_utils/toy_datasets.py
+++ summarization_utils/toy_datasets.py
@@ -1608,6 +1608,97 @@ class ToyProfilesTemplate(TemplaticDataset):
         ]
 
 
+class WalkedToTemplate(TemplaticDataset):
+    PLACES = ["store", "gym", "house", "library", "office", "park", "school"]
+    ACTIONS = [
+        "walked across town",
+        "ran across town",
+        "drove across town",
+        "needs to go",
+        "likes to go",
+        "wants to go",
+    ]
+
+    @classmethod
+    def is_moving(cls, action: str) -> bool:
+        return "walk" in action or "ran" in action or "drove" in action
+
+    def __init__(
+        self, model: HookedTransformer, dataset_size: int | None = None, seed: int = 0
+    ) -> None:
+        template = (
+            wrap_instruction(
+                "Anne was at the {PLACE0}. She {ACTION1} to the {PLACE1}. Next, she {ACTION2} to the {PLACE2}. Where is Anne right now?",
+                model,
+            )
+            + "Anne is at the"
+        )
+        prompt_tuples = [
+            (place0, action1, place1, action2, place2)
+            for place0, place1, place2 in itertools.combinations(self.PLACES, 3)
+            for action1, action2 in itertools.product(self.ACTIONS, repeat=2)
+        ]
+        random.seed(seed)
+        random.shuffle(prompt_tuples)
+        super().__init__(template, prompt_tuples, model, dataset_size)
+        self.seed = seed
+
+    def get_answers(self, prompt_tuples: List[Tuple[str, ...]]):
+        answers = []
+        for _, (place0, action1, place1, action2, place2) in enumerate(prompt_tuples):
+            if self.is_moving(action2):
+                place = place2
+            elif self.is_moving(action1):
+                place = place1
+            else:
+                place = place0
+            answers.append(" " + place)
+
+    def get_counterfactual_tuples(self) -> List[Tuple[str, ...]]:
+        """Flip the answer by changing action1 or action2"""
+        cf_tuples = []
+        for _, (place0, action1, place1, action2, place2) in enumerate(
+            self.prompt_tuples
+        ):
+            if self.is_moving(action2):
+                # In order to flip the answer, we need to make sure that action2 is not moving
+                new_action2 = random.choice(
+                    [a for a in self.ACTIONS if not self.is_moving(a)]
+                )
+                cf_tuples.append((place0, action1, place1, new_action2, place2))
+            elif self.is_moving(action1):
+                # In order to flip the answer, we need to make sure that action1 is not moving
+                new_action1 = random.choice(
+                    [a for a in self.ACTIONS if not self.is_moving(a)]
+                )
+                cf_tuples.append((place0, new_action1, place1, action2, place2))
+            else:
+                # In order to flip the answer, we must make either action1 or action2 move
+                if random.choice([True, False]):
+                    new_action1 = random.choice(
+                        [a for a in self.ACTIONS if self.is_moving(a)]
+                    )
+                    cf_tuples.append((place0, new_action1, place1, action2, place2))
+                else:
+                    new_action2 = random.choice(
+                        [a for a in self.ACTIONS if self.is_moving(a)]
+                    )
+                    cf_tuples.append((place0, action1, place1, new_action2, place2))
+        return cf_tuples
+
+    def format_prompts(self, prompt_tuples: List[Tuple[str, ...]]):
+        return [
+            self.template.format(
+                PLACE0=place0,
+                ACTION1=action1,
+                PLACE1=place1,
+                ACTION2=action2,
+                PLACE2=place2,
+            )
+            for place0, action1, place1, action2, place2 in prompt_tuples
+        ]
+
+
 class CounterfactualDataset:
     def __init__(
         self,
@@ -1840,6 +1931,8 @@ class CounterfactualDataset:
             return ToyDeductionTemplate(model, **kwargs).to_counterfactual()
         elif name == "ToyProfiles":
             return ToyProfilesTemplate(model, **kwargs).to_counterfactual()
+        elif name == "WalkedTo":
+            return WalkedToTemplate(model, **kwargs).to_counterfactual()
         else:
             raise ValueError(f"Unknown dataset {name} for model {model.cfg.model_name}")
 
